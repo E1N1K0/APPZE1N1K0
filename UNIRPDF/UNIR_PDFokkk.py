52d5ad0ef5aa44412e54d0bdf87b08ad36403069
Nicolas Daroch <69065853+E1N1K0@users.noreply.github.com>
2026-05-10 18:38:22 -0400

    Update UNIR_PDFokkk.py

--- UNIRPDF/UNIR_PDFokkk.py
+++ UNIRPDF/UNIR_PDFokkk.py
@@ -1,14 +1,24 @@
-#Unir Pdf:
+"""
+@author: E1n1k0programa@gmail.com
 
-#Primero instalar librerias! pip install PyPDF4
-from PyPDF4 import PdfFileMerger
+Si no esta instalado, escribe pip install PyPDF2, en el terminal y ejecútalo.
+Coloca todos los archivos a unir en la misma carpeta que el script.
+"""
 
-merger = PdfFileMerger()
+from PyPDF2 import PdfMerger
 
-#Agrega los pdf a unir:
-merger.append('imagen1.pdf')
-merger.append('imagen2.pdf')
+# Crear el objeto mezclador
+merger = PdfMerger()
 
-#Genera archivo pf final:
-merger.write('pdf_unido.pdf')
-merger.close
+# Lista de archivos a unir (escritos manualmente)
+archivos = ["imagen1.pdf", "imagen2.pdf"]
+
+# Recorrer la lista y agregar cada PDF
+for pdf in archivos:
+    merger.append(pdf)
+
+# Guardar el resultado final
+merger.write("PDF_Unido.pdf")
+merger.close()
+
+print("¡PDFs unidos exitosamente!")
